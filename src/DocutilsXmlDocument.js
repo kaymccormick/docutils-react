37ec75c2dc9fb166c627344e74f880725d9d9a4e
Kay <kay@kaymccormick.com>
2019-04-20 17:05:28 +0000

    autofix after adding jsx to eslint config

--- src/DocutilsXmlDocument.js
+++ src/DocutilsXmlDocument.js
@@ -2,21 +2,21 @@
 /**
  * This file is antiquiated
  */
-import { jsx } from '@emotion/core'
-import PropTypes from 'prop-types'
+import { jsx } from '@emotion/core';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { setupSaxParser, getComponentForTagName, attributesToProps } from './getComponentForXmlSax';
-import "@babel/polyfill";
-//import './DocutilsXmlDocument.css';
+import '@babel/polyfill';
+// import './DocutilsXmlDocument.css';
 import nodeFetch from 'node-fetch';
 
 async function myfunc(reader, parser) {
-    while(true) {
+    while (true) {
         const { done, value } = await reader.read();
-        if(done) {
+        if (done) {
             break;
         }
-        const val = new TextDecoder("utf-8").decode(value);
+        const val = new TextDecoder('utf-8').decode(value);
 //        console.log(val);
         parser.write(val);
     }
@@ -24,24 +24,24 @@ async function myfunc(reader, parser) {
 
 function myfunc2(stream, parser) {
     let chunk;
-    while(null !== (chunk = stream.read())) {
+    while ((chunk = stream.read()) !== null) {
 //      console.log(chunk);
         parser.write(chunk);
     }
 }
 
 function handleNativeFetchResponse(parser) {
-    return response => {
+    return (response) => {
         const reader = response.body.getReader();
         return myfunc(reader, parser).then(() => {
             reader.releaseLock();
             parser.close();
-        })
+        });
     };
 }
 
 function handleNodeFetchResponse(parser) {
-    return response => {
+    return (response) => {
         response.body.setEncoding('utf8');
         response.body.on('readable', () => myfunc2(response.body, parser));
         response.body.on('end', () => parser.close());
@@ -51,28 +51,26 @@ function handleNodeFetchResponse(parser) {
 class DocutilsXmlDocument extends Component {
     constructor(props) {
         super(props);
-        this.state = {xmlDocument: null, component: null, loading: true};
+        this.state = { xmlDocument: null, component: null, loading: true };
         this.updateDocument();
     }
-    
-    componentDidUpdate(prevProps, prevState)
-    {
+
+    componentDidUpdate(prevProps, prevState) {
         console.log(`componentDidUpdate ${prevProps.docName} !== ? ${this.props.docName}`);
-        if(prevProps.docName !== this.props.docName) {
+        if (prevProps.docName !== this.props.docName) {
             /* Update document */
             this.updateDocument();
         }
     }
 
     componentWillUnmount() {
-        if(this.timerId) {
+        if (this.timerId) {
             clearInterval(this.timerId);
             this.timerId = undefined;
         }
     }
 
-    updateDocument()
-    {
+    updateDocument() {
         console.log('updateDocument');
         const context = { getComponent: this.props.getComponent };
         // extraProps?
@@ -82,48 +80,48 @@ class DocutilsXmlDocument extends Component {
             console.log('end');
             const nodes = context.siblings[0].map(f => f());
             const r = nodes.filter(React.isValidElement)[0];
-            if(!React.isValidElement(r)) {
+            if (!React.isValidElement(r)) {
                 console.dir(r);
                 console.log('invalid element');
                 return;
             }
             const data = context.nodes[0].dataChildren.map(f => f()).filter(e => e[0] === 'document')[0];
-            if(this.props.handleData) {
+            if (this.props.handleData) {
                 this.props.handleData(data);
             }
-            this.setState({component: r, loading: false}, () => {
+            this.setState({ component: r, loading: false }, () => {
                 console.log('state has been set');
-                if(me.timerId) {
+                if (me.timerId) {
                     clearInterval(me.timerId);
                     this.timerId = undefined;
                 }
-                this.props.onComplete({ docName: me.props.docName});
+                this.props.onComplete({ docName: me.props.docName });
             });
-            
-        }).bind(this);
+        });
         me.count = 0;
         //        this.timerId = setInterval(() => intervalFunc(me, context), 250);
         this.props.getDocumentStream({ docName: this.props.docName }).then(
-            this.props.handleDocumentStream)
-            .catch(err => {
-                if(me.timerId) {
+            this.props.handleDocumentStream,
+)
+            .catch((err) => {
+                if (me.timerId) {
                     clearInterval(me.timerId);
                     me.timerId = undefined;
                 }
                 console.log(`get stream error ${this.props.docName}`);
-                if(err.stack) {
+                if (err.stack) {
                     console.log(err.stack);
                 }
                 console.log(err);
-                this.setState({loading: false});
-                if(this.props.onFail) {
+                this.setState({ loading: false });
+                if (this.props.onFail) {
                     this.props.onFail(this.props.docName, err);
                 }
             });
     }
-    
+
     render() {
-        return (!this.state.loading) ? <div onClick={this.props.onClick}><div className="info-panel p-grid"><div className="p-col">Document Name:</div><div className="p-col">{this.props.docName}</div></div><div>{this.state.component}</div></div> : this.props.loadingRender({loading: this.state.loading});
+        return (!this.state.loading) ? <div onClick={this.props.onClick}><div className="info-panel p-grid"><div className="p-col">Document Name:</div><div className="p-col">{this.props.docName}</div></div><div>{this.state.component}</div></div> : this.props.loadingRender({ loading: this.state.loading });
     }
 }
 
--- src/Viewer.js
+++ src/Viewer.js
@@ -1,48 +1,48 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
-import PropTypes from 'prop-types'
+import { jsx } from '@emotion/core';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import styled from '@emotion/styled';
 import DocutilsXmlDocument from './DocutilsXmlDocument';
-import {getComponent as wrappedGetComponent} from './docutilsWrapper';
+import { getComponent as wrappedGetComponent } from './docutilsWrapper';
 
-const handleNewDocument = (app) => (o) => {
+const handleNewDocument = app => (o) => {
     console.log('handleNewDocument');
     return new Promise((resolve, reject) => {
         app.setState(o, () => { resolve(); });
     });
 };
 
-const resolveUriRef = function(curDocName, href) {
+const resolveUriRef = function (curDocName, href) {
     const fragmentIndex = href.indexOf('#');
     let fragment;
     let docName;
-    if(fragmentIndex !== -1) {
+    if (fragmentIndex !== -1) {
         fragment = href.substring(fragmentIndex + 1);
         docName = href.substring(0, fragmentIndex);
     } else {
         docName = href;
     }
-    
-    let slash = curDocName.lastIndexOf('/');
+
+    const slash = curDocName.lastIndexOf('/');
     if (slash !== -1) {
         curDocName = curDocName.substring(0, slash);
     } else {
-        curDocName = "";
+        curDocName = '';
     }
-    while(docName.startsWith('../')) {
+    while (docName.startsWith('../')) {
         docName = docName.substring(3);
         const slash = curDocName.lastIndexOf('/');
         curDocName = curDocName.substring(0, slash);
     }
-    if(docName) {
-        docName = (curDocName ? curDocName + '/' : '') + docName;
+    if (docName) {
+        docName = (curDocName ? `${curDocName}/` : '') + docName;
     }
     return { fragmentId: fragment, docName };
-}
+};
 
 const getXmlDocumentComponent = (xmlData, extraProps, config) => {
-    if(!config) {
+    if (!config) {
         config = {};
     }
     return getComponentForXml(xmlData, { ...config, extraProps });
@@ -54,16 +54,16 @@ function intervalFunc(me, context) {
     const tags = context.tags.slice();
     const nodes = context.nodes.slice();
 //    console.log(`${me.count}: ${tags.join(' > ')}`);
-    let i = context.tags.length -1;
+    let i = context.tags.length - 1;
     let tag;
     let node;
-    let lastComponent = undefined;
-    while((node = nodes.pop()) && node.name) {
+    let lastComponent;
+    while ((node = nodes.pop()) && node.name) {
         // console.dir(siblings);
         const Component = getComponentForTagName(node.name);
-        if(Component) {
-            let siblings = context.siblings[i].map(f => f());
-            if(lastComponent) {
+        if (Component) {
+            const siblings = context.siblings[i].map(f => f());
+            if (lastComponent) {
                 siblings.push(lastComponent);
             }
             // console.log(`length of context.attributes ${context.attributes.length}`);
@@ -71,25 +71,24 @@ function intervalFunc(me, context) {
             const a1 = node.attributes;
             // console.dir(a1);
             const att = attributesToProps(a1);
-            if(tag === "DocumentLink") {
+            if (tag === 'DocumentLink') {
                 // console.dir(a1);
             }
             const c = <Component {...att} children={siblings}/>;
             lastComponent = c;
         } else {
-            throw new Error("Invalid component");
+            throw new Error('Invalid component');
         }
         i--;
     }
     const r = lastComponent;
-    if(React.isValidElement(r)) {
+    if (React.isValidElement(r)) {
 //      console.log('setting state component to');
 //      console.dir(r);
 //      console.dir(renderToString(r));
-        
-        me.setState({component: r}, () => console.log('updated component at inteval'));
-    }
-    else {
+
+        me.setState({ component: r }, () => console.log('updated component at inteval'));
+    } else {
 //      me.setState({component: <div>Invalid component</div>});
     }
 }
@@ -100,49 +99,48 @@ class Viewer extends Component {
         this.handleNewDocument = (handleNewDocument(this)).bind(this);
         this.referenceOnClick = this.referenceOnClick.bind(this);
         this.handleToctreeData = this.handleToctreeData.bind(this);
-        if(props.getDocumentUrl) {
+        if (props.getDocumentUrl) {
             this.getDocumentUrl = props.getDocumentUrl;
         } else {
             this.getDocumentUrl = this.getDocumentUrl.bind(this);
         }
         this.state = { docName: 'index', loading: true };
     }
-    
-    navigateToDocument(docName, fragmentId)
-    {
-        return this.handleNewDocument({docName, fragmentId});
+
+    navigateToDocument(docName, fragmentId) {
+        return this.handleNewDocument({ docName, fragmentId });
     }
 
     referenceOnClick(o) {
         return ((e) => {
             const getCurrentDocumentName = o.getCurDocName;
-            console.log("referenceOnClick");
-            if(e.target.tagName.toLowerCase() !== 'a') {
+            console.log('referenceOnClick');
+            if (e.target.tagName.toLowerCase() !== 'a') {
                 return;
             }
             e.preventDefault();
             const href = e.target.getAttribute('href');
             console.log(`in referenceOnClick, href is ${href}`);
-            if (! href ) {
+            if (!href) {
                 e.preventDefault();
-                console.log("No href, returning");
-                return;
-            }
-            
-            if(href.includes(':')) {
-                console.log("Xternal link");
+                console.log('No href, returning');
                 return;
             }
 
-            const curDocName = getCurrentDocumentName() || "";
+            if (href.includes(':')) {
+                console.log('Xternal link');
+                return;
+            }
+
+            const curDocName = getCurrentDocumentName() || '';
             console.log(`curDocName is ${curDocName}`);
             return;
             const resolved = resolveUriRef(curDocName, href);
             const fragmentId = resolved.fragmentId || '';
             const docName = resolved.docName;
 
-            if(docName === "") {
-                console.log("reference to element in current document #" + fragmentId);
+            if (docName === '') {
+                console.log(`reference to element in current document #${fragmentId}`);
                 //      app.setState({fragmentId});
             } else {
                 console.log(`derived document name is ${docName}`);
@@ -154,109 +152,123 @@ class Viewer extends Component {
                           console.log('navigation complete');
                           }).catch(err => {
                           console.log('Unable to navigate to document ' + docName + ': ' + err);
-                          });*/
+                          }); */
             }
-        }).bind(this);
+        });
     }
 
     getDocName() {
-        return this.props.match && this.props.match.params ? (this.props.match.params.docName || 'index'): 'index';
+        return this.props.match && this.props.match.params ? (this.props.match.params.docName || 'index') : 'index';
     }
-    
+
     getDocumentUrl() {
-        return (docName => document.location.origin + (this.props.src || '') + '/' + docName + '.xml').bind(this);
+        return (docName => `${document.location.origin + (this.props.src || '')}/${docName}.xml`);
     }
-    
+
 
     doneLoading(...a) {
         const onComplete = this.props.onComplete;
-        if(onComplete) {
-            this.setState({loading: false}, () => onComplete(...a));
+        if (onComplete) {
+            this.setState({ loading: false }, () => onComplete(...a));
         } else {
-            this.setState({loading: false});
+            this.setState({ loading: false });
         }
     }
 
     handleToctreeData(data) {
-        if(this.props.handleToctreeData) {
+        if (this.props.handleToctreeData) {
             this.props.handleToctreeData(data);
         }
     }
-    
+
     render() {
-        let linksComplete = false, docComplete = false;
-        let linksFail= false, docFail = false;
+        let linksComplete = false; let
+docComplete = false;
+        let linksFail = false; let
+docFail = false;
         const onComplete = this.props.onComplete;
         const me = this;
 
         function handleLinksComplete(...a) {
             console.log('links complete');
             linksComplete = true;
-            if((docComplete || docFail)) {
-                me.doneLoading({ docComplete,
-                                 linksComplete }, ...a);
+            if ((docComplete || docFail)) {
+                me.doneLoading({
+ docComplete,
+                                 linksComplete,
+}, ...a);
             }
         }
 
         function handleLinksFail(...a) {
             console.log('links fail');
             linksFail = true;
-            if((docComplete || docFail)) {
-                me.doneLoading({ docComplete,
-                                 linksComplete }, ...a);
+            if ((docComplete || docFail)) {
+                me.doneLoading({
+ docComplete,
+                                 linksComplete,
+}, ...a);
             }
         }
 
         function handleDocComplete(...a) {
             console.log('doc complete');
             docComplete = true;
-            if((linksComplete || linksFail)) {
-                me.doneLoading({ docComplete,
-                                 linksComplete }, ...a);
+            if ((linksComplete || linksFail)) {
+                me.doneLoading({
+ docComplete,
+                                 linksComplete,
+}, ...a);
             }
         }
 
         function handleDocFail(...a) {
             console.log('doc fail');
             docFail = true;
-            if((linksComplete || linksFail)) {
-                me.doneLoading({ docComplete,
-                                 linksComplete }, ...a);
+            if ((linksComplete || linksFail)) {
+                me.doneLoading({
+ docComplete,
+                                 linksComplete,
+}, ...a);
             }
         }
 
         return (this.props.children(<DocutilsXmlDocument
                                     getDocumentStream={this.props.getDocumentStream}
       handleData={this.handleToctreeData}
-      css={{margin: '.8rem', border: 'dashed 1px #888888'}}
+      css={{ margin: '.8rem', border: 'dashed 1px #888888' }}
       getComponent={this.props.getComponent}
       referenceTemplate={this.props.referenceTemplate}
       match={this.props.match} location={this.props.location} history={this.props.history}
-      onClick={this.referenceOnClick({navigateToDocument: this.navigateToDocument.bind(this),
-      getCurDocName: () => '_links'})}
+      onClick={this.referenceOnClick({
+ navigateToDocument: this.navigateToDocument.bind(this),
+      getCurDocName: () => '_links',
+})}
                                     getDocumentUrl={() => this.getDocumentUrl('_links')}
       onFail={handleLinksFail}
       onComplete={handleLinksComplete} extraProps={{}}
       getXmlDocumentComponent={getXmlDocumentComponent}
-        loadingRender={({loading}) => 'Loading'}
+        loadingRender={({ loading }) => 'Loading'}
                                     docName="_links"/>,
       <DocutilsXmlDocument
                                     getDocumentStream={this.props.getDocumentStream}
         getComponent={this.props.getComponent}
-        css={{margin: '.8rem', border: 'dashed 1px #888888'}}
+        css={{ margin: '.8rem', border: 'dashed 1px #888888' }}
         referenceTemplate={this.props.referenceTemplate}
         match={this.props.match} location={this.props.location} history={this.props.history}
-        onClick={this.referenceOnClick({navigateToDocument: this.navigateToDocument.bind(this),
-        getCurDocName: () => this.state.docName})}
+        onClick={this.referenceOnClick({
+ navigateToDocument: this.navigateToDocument.bind(this),
+        getCurDocName: () => this.state.docName,
+})}
                                     getDocumentUrl={() => this.getDocumentUrl(this.getDocName())}
         ref={this.mainRef}
         onFail={handleDocFail}
         onComplete={handleDocComplete}
-        css={{display: 'grid', gridTemplateAreas: ['"rellinks main"']}}
+        css={{ display: 'grid', gridTemplateAreas: ['"rellinks main"'] }}
         extraProps={{}}
         getXmlDocumentComponent={getXmlDocumentComponent}
-        loadingRender={({loading}) => 'Loading'}
-        docName={this.props.docName||'index'}
+        loadingRender={({ loading }) => 'Loading'}
+        docName={this.props.docName || 'index'}
                                     />));
     }
 }
@@ -276,4 +288,3 @@ Viewer.propTypes = {
 };
 
 export default Viewer;
-
