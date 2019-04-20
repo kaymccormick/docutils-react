d0104669c29a59d32c41ec246c010efff19d39c2
Kay <kay@kaymccormick.com>
2019-04-20 17:04:40 +0000

    add jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
